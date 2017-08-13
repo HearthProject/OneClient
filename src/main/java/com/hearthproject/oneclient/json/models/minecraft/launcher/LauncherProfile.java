430f621097afab6b95fa605bd5beba7c468e166b
modmuss50 <modmuss50@gmail.com>
2017-08-13 19:28:28 +0100

    Auto selects the forge profile

---
+++ src/main/java/com/hearthproject/oneclient/json/models/minecraft/launcher/LauncherProfile.java
@@ -0,0 +1,30 @@
+package com.hearthproject.oneclient.json.models.minecraft.launcher;
+
+import java.util.Map;
+
+public class LauncherProfile {
+
+	public String selectedProfile, clientToken, selectedUser;
+
+	public Map<String, Profile> profiles;
+
+	public Map<String, AuthenticationDatabase> authenticationDatabase;
+
+	public static class Profile {
+
+		public Profile(String name, String lastVersionId) {
+			this.name = name;
+			this.lastVersionId = lastVersionId;
+		}
+
+		public Profile() {
+		}
+
+		public String name;
+		public String lastVersionId;
+	}
+
+	public class AuthenticationDatabase {
+		String displayName, accessToken, userid, uuid, username;
+	}
+}
--- src/main/java/com/hearthproject/oneclient/util/minecraft/MinecraftUtil.java
+++ src/main/java/com/hearthproject/oneclient/util/minecraft/MinecraftUtil.java
@@ -5,6 +5,7 @@ import com.hearthproject.oneclient.Constants;
 import com.hearthproject.oneclient.json.JsonUtil;
 import com.hearthproject.oneclient.json.models.launcher.Instance;
 import com.hearthproject.oneclient.json.models.minecraft.GameVersion;
+import com.hearthproject.oneclient.json.models.minecraft.launcher.LauncherProfile;
 import com.hearthproject.oneclient.util.forge.ForgeUtils;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
@@ -14,6 +15,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 
 public class MinecraftUtil {
 
@@ -36,12 +38,41 @@ public class MinecraftUtil {
 		File launcher = new File(Constants.RUNDIR, "launcher.jar");
 		File mcDir = new File(Constants.RUNDIR, "minecraft");
 		if(!launcher.exists()){ //TODO check hash
+			System.out.println("Downloading minecraft launcher");
 			FileUtils.copyURLToFile(new URL("http://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar"), launcher);
 		}
 
+		System.out.println("Creating launcher json");
+		checkLauncherProfiles(mcDir, new LauncherProfile.Profile("TestProfile", "1.12.1-forge1.12.1-14.22.0.2452"));
+
+		System.out.println("Downloading forge installer");
 		ForgeUtils.installForge(mcDir, "1.12.1-14.22.0.2452");
 		ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", launcher.getAbsolutePath(), "--workDir", mcDir.getAbsolutePath());
 		processBuilder.start();
+	}
+
+	public static void checkLauncherProfiles(File mcDir, LauncherProfile.Profile profile) throws IOException {
+		File launcherProfiles = new File(mcDir,"launcher_profiles.json");
+
+		LauncherProfile launcherProfile;
+		if(launcherProfiles.exists()){
+			launcherProfile = JsonUtil.GSON.fromJson(FileUtils.readFileToString(launcherProfiles, StandardCharsets.UTF_8), LauncherProfile.class);
+		} else {
+			launcherProfile = new LauncherProfile();
+		}
+
+		if(launcherProfile.profiles == null){
+			launcherProfile.profiles = new HashMap<>();
+		}
+		if(launcherProfile.profiles.containsKey(profile.name)){
+			launcherProfile.profiles.get(profile.name).lastVersionId = profile.lastVersionId;
+		} else {
+			launcherProfile.profiles.put(profile.name, profile);
+		}
+
+		launcherProfile.selectedProfile = profile.name;
+
+		FileUtils.writeStringToFile(launcherProfiles, JsonUtil.GSON.toJson(launcherProfile));
 	}
 
 	public static void main(String[] args) throws IOException {
