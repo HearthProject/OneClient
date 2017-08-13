ec8165e57a9013e7d5a1d78c591dafe75ccbd40f
modmuss50 <modmuss50@gmail.com>
2017-08-13 16:37:13 +0100

    Basic work on instances

---
+++ src/main/java/com/hearthproject/oneclient/json/models/launcher/Instance.java
@@ -0,0 +1,25 @@
+package com.hearthproject.oneclient.json.models.launcher;
+
+public class Instance {
+
+	public String name;
+	//This is here to allow support for more than just mc in the future
+	public String game = "Minecraft";
+
+	public MinecraftData minecraftData;
+
+	public String gameDir;
+
+	public String icon;
+
+	public long lastLaunch;
+
+	public long playTime;
+
+	public class MinecraftData {
+		String gameVersion;
+
+		String modLoader;
+	}
+
+}
---
+++ src/main/java/com/hearthproject/oneclient/json/models/launcher/InstanceList.java
@@ -0,0 +1,12 @@
+package com.hearthproject.oneclient.json.models.launcher;
+
+import java.util.List;
+
+public class InstanceList {
+
+	public List<Instance> instances;
+
+	//the name of the last played instance, may be null
+	public String lastUsedInstance;
+
+}
