008c3445645d618cda0ea2df7f98b3395146f380
Prospector <thedoctorsoda@gmail.com>
2017-08-13 07:04:48 -0700

    Add some basic buttons

--- src/main/java/com/hearthproject/oneclient/Controller.java
+++ src/main/java/com/hearthproject/oneclient/Controller.java
@@ -1,11 +1,14 @@
 package com.hearthproject.oneclient;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-import java.awt.*;
 
 public class Controller {
     @FXML
@@ -14,8 +17,28 @@ public class Controller {
     public ScrollPane scrollPane;
     @FXML
     public TilePane instancePane;
+    @FXML
+    public ProgressBar progressBar;
+    @FXML
+    public ChoiceBox instanceChoiceBox;
+    @FXML
+    public Button playButton;
+    @FXML
+    public Button newInstanceButton;
 
     public void onStart(Stage stage) {
         tabBar.setWidth(Integer.MAX_VALUE);
+        setProgress(40);
+    }
+
+    public void onNewInstancePress() {
+
+    }
+
+    public void onPlayPress() {
+    }
+
+    public void setProgress(double percent) {
+        progressBar.setProgress(Double.parseDouble("0." + String.valueOf(percent).replace(".", "")));
     }
 }
