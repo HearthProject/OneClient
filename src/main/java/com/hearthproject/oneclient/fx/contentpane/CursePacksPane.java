076551bff90039c9f10918ad3a2b8c93f18f0440
Prospector <thedoctorsoda@gmail.com>
2017-08-18 12:14:29 -0700

    Fix web view width/height

--- src/main/java/com/hearthproject/oneclient/fx/contentpane/CursePacksPane.java
+++ src/main/java/com/hearthproject/oneclient/fx/contentpane/CursePacksPane.java
@@ -1,5 +1,6 @@
 package com.hearthproject.oneclient.fx.contentpane;
 
+import com.hearthproject.oneclient.Main;
 import com.hearthproject.oneclient.fx.contentpane.base.ContentPane;
 import javafx.scene.web.WebView;
 
@@ -7,12 +8,14 @@ public class CursePacksPane extends ContentPane {
 	public WebView webView;
 
 	public CursePacksPane() {
-		super("gui/contentpanes/getCurseContent.fxml", "Get Curse Pack", "#3A54A3");
+		super("gui/contentpanes/getCurseContent.fxml", "Get Curse Pack", "#DB6B20");
 	}
 
 	@Override
 	protected void onStart() {
 		webView.getEngine().load("https://minecraft.curseforge.com/modpacks");
+		webView.minWidthProperty().bind(Main.mainController.contentBox.widthProperty());
+		webView.minHeightProperty().bind(Main.mainController.contentBox.heightProperty());
 	}
 
 	@Override
