eeb4ed8c76a469546d2f9fd40bf5fbcb55fd51d2
modmuss50 <modmuss50@gmail.com>
2017-08-27 14:43:24 +0100

    Add openjfx check

--- src/main/java/com/hearthproject/oneclient/Launch.java
+++ src/main/java/com/hearthproject/oneclient/Launch.java
@@ -1,10 +1,23 @@
 package com.hearthproject.oneclient;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class Launch {
 	public static void main(String[] args) throws IOException {
-		//TODO check openJFX
+		try {
+			Class.forName("javafx.stage.Stage");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null,
+				"JavaFX wasn't found installed on this computer, this is usually because you are using OpenJDK that does not include it, to fix this error install openjfx for your distribution."
+					+ "\nOn debian based distributions this can fixed by running"
+					+ "\n   sudo apt-get install openjfx"
+					+ "\n   or by installing oracle java"
+					+ "\nIf you are not using OpenJDK something has gone badly wrong, please contact the OneClient Developers",
+				"A cataclysmic error has occurred!",
+				JOptionPane.ERROR_MESSAGE);
+		}
 		Main.main(args);
 	}
 
