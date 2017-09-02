87fdc4593d59c4fe54dae3ebf3f605890885f2e9
primetoxinz <primetoxinzz@gmail.com>
2017-09-01 21:41:46 -0400

    Fix Memory spinboxes.

--- src/main/java/com/hearthproject/oneclient/fx/contentpane/SettingsPane.java
+++ src/main/java/com/hearthproject/oneclient/fx/contentpane/SettingsPane.java
@@ -47,8 +47,15 @@ public class SettingsPane extends ContentPane {
 		});
 
 		maxMemory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, (int) OperatingSystem.getOSTotalMemory(), SettingsUtil.settings.minecraftMaxMemory, 128);
-		minMemory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, maxMemory.getValue(), SettingsUtil.settings.minecraftMinMemory, 128);
-		maxMemory.valueProperty().addListener((observableValue, a, b) -> minMemory.setMax(maxMemory.getValue()));
+		minMemory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, (int) OperatingSystem.getOSTotalMemory(), SettingsUtil.settings.minecraftMinMemory, 128);
+		maxMemory.valueProperty().addListener((observableValue, a, b) -> {
+			if (minMemory.getValue() > maxMemory.getValue())
+				minMemory.setValue(maxMemory.getValue());
+		});
+		minMemory.valueProperty().addListener((observableValue, a, b) -> {
+			if (minMemory.getValue() > maxMemory.getValue())
+				maxMemory.setValue(minMemory.getValue());
+		});
 
 		spinnerMinRAM.setValueFactory(minMemory);
 		spinnerMaxRAM.setValueFactory(maxMemory);
